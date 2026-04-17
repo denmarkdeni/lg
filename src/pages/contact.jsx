686460772752b45b378b137e6527a2d824606496
mariadeniston <mariadeniston111@gmail.com>
2026-04-17 16:11:16 +0530

    ok

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,15 @@
 import { usePopup } from "../hooks/usePopup";
+import Branches from "../sections/branches";
 import ContactBanner from "../sections/contact_banner";
+import Hero from "../sections/hero";
 
 export default function Contact() {
     const page = "Contact Page";
     const { popup, openPopup, closePopup } = usePopup();
     return (  
-      <>  
+      <>    
+        <Hero page={page} openPopup={openPopup}/> 
+        <Branches />
         <ContactBanner page={page} openPopup={openPopup}/>
       </>
     );
